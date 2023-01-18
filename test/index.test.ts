b6fd4764d0e18ff8f00c41bec08aa3ef71c07f8d
s-sasaki-0529 <shingo.sasaki.0529@gmail.com>
2023-01-19 00:11:58 +0900

    restructure test code

--- test/index.test.ts
+++ test/index.test.ts
@@ -2,53 +2,61 @@ import { describe, expect, test } from 'vitest'
 import saxios from '../src/index'
 
 describe('saxios', () => {
-  describe('basic request methods', () => {
-    test('#request', async () => {
-      const res = await saxios.request('http://127.0.0.1:3000/')
-      const text = await res.text()
-      expect(text).toEqual('GET /')
+  describe('instance methods', () => {
+    describe('#get', () => {
+      test('basic request', async () => {
+        const res = await saxios.request('http://127.0.0.1:3000/')
+        const text = await res.text()
+        expect(text).toEqual('GET /')
+      })
     })
 
-    test('#get', async () => {
-      const res = await saxios.get('http://127.0.0.1:3000/')
-      const text = await res.text()
-      expect(text).toEqual('GET /')
+    describe('#post', () => {
+      test('basic request', async () => {
+        const res = await saxios.post('http://127.0.0.1:3000/')
+        const text = await res.text()
+        expect(text).toEqual('POST /')
+      })
     })
 
-    test('#post', async () => {
-      const res = await saxios.post('http://127.0.0.1:3000/')
-      const text = await res.text()
-      expect(text).toEqual('POST /')
+    describe('#put', () => {
+      test('basic request', async () => {
+        const res = await saxios.put('http://127.0.0.1:3000/')
+        const text = await res.text()
+        expect(text).toEqual('PUT /')
+      })
     })
 
-    test('#put', async () => {
-      const res = await saxios.put('http://127.0.0.1:3000/')
-      const text = await res.text()
-      expect(text).toEqual('PUT /')
+    describe('#patch', () => {
+      test('basic request', async () => {
+        const res = await saxios.patch('http://127.0.0.1:3000/')
+        const text = await res.text()
+        expect(text).toEqual('PATCH /')
+      })
     })
 
-    test('#patch', async () => {
-      const res = await saxios.patch('http://127.0.0.1:3000/')
-      const text = await res.text()
-      expect(text).toEqual('PATCH /')
+    describe('#delete', () => {
+      test('basic request', async () => {
+        const res = await saxios.delete('http://127.0.0.1:3000/')
+        const text = await res.text()
+        expect(text).toEqual('DELETE /')
+      })
     })
 
-    test('#delete', async () => {
-      const res = await saxios.delete('http://127.0.0.1:3000/')
-      const text = await res.text()
-      expect(text).toEqual('DELETE /')
+    describe('#head', () => {
+      test('basic request', async () => {
+        const res = await saxios.head('http://127.0.0.1:3000/')
+        const header = res.headers.get('x-test')
+        expect(header).toEqual('HEAD /')
+      })
     })
 
-    test('#head', async () => {
-      const res = await saxios.head('http://127.0.0.1:3000/')
-      const header = res.headers.get('x-test')
-      expect(header).toEqual('HEAD /')
-    })
-
-    test('#options', async () => {
-      const res = await saxios.options('http://127.0.0.1:3000/')
-      const text = await res.text()
-      expect(text).toEqual('OPTIONS /')
+    describe('#options', () => {
+      test('basic request', async () => {
+        const res = await saxios.options('http://127.0.0.1:3000/')
+        const text = await res.text()
+        expect(text).toEqual('OPTIONS /')
+      })
     })
   })
 })
